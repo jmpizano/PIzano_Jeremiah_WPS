32c4fdaafb037c96304bb0e235afc53e57bee276
jmpizano <jeremiah.pizano@gmail.com>
2015-03-05 13:12:02 -0800

    Output Assignment
    Here are the files to my assignment for week one.

---
+++ Pizano_Jeremiah_Output_Assignment/js/script.js
@@ -0,0 +1,17 @@
+/**
+ * Created by JP on 3/5/15.
+ */
+// Jeremiah Pizano
+    // Date: 03-05-2015
+    // Class: SDI
+// Assignment: Output
+
+// variables
+var mySchool = "Riverbank High School."; // This will display what high school I graduated from.
+var yearsToGraduate = 3; // This will display how long I attended high school.
+var myMajor = true; // I do attend Full Sail.
+
+//display
+console.log("It is " + myMajor + " That I am currently enrolled in Full Sail University.");
+console.log("Before I came to FUll Sail, I attended " + mySchool);
+console.log("I was able to graduate high school in " + yearsToGraduate + " years.");
