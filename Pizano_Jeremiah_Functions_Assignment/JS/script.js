e74dd3c05ca87418a50828a598ba383061ebf75f
jmpizano <jeremiah.pizano@gmail.com>
2015-03-26 15:43:23 -0700

    Functions v3
    This contains the complete function that produces either the Florida
    lottery numbers or the Powerball numbers.

--- Pizano_Jeremiah_Functions_Assignment/JS/script.js
+++ Pizano_Jeremiah_Functions_Assignment/JS/script.js
@@ -8,8 +8,8 @@
 
 // variables
 
-//var lotteryChoice = prompt("Which lotto numbers would you like to see?\n\nPress 1 for the Florida State lottery numbers.\nPress 2 for the Powerball lottery numbers."); //This will ask the user which lotto numbers they want to see.
-
+var lotteryChoice = prompt("Which lotto numbers would you like to see?\n\nPress 1 for the Florida State lottery numbers.\nPress 2 for the Powerball lottery numbers."); //This will ask the user which lotto numbers they want to see.
+var number;
 
 // functions
 function lottoValidation(chooseALotto) { //This function will verify that the user entered a choice.
@@ -19,22 +19,41 @@ function lottoValidation(chooseALotto) { //This function will verify that the us
     return chooseALotto; //This will return the choice that the user entered into the prompt.
 }
 
-function numberGen() {
-    var randomArray = [];
-    var myRandomNumber = Math.round(Math.random() * (53 - 1) + 1);
+function numberGen() { // 
+
+    //Florida Numbers
     var floridaLottery = Math.round(Math.random() * (53 - 1) + 1);
+    var floridaLotteryTwo = Math.round(Math.random() * (53 - 1) + 1);
+    var floridaLotteryThree = Math.round(Math.random() * (53 - 1) + 1);
+    var floridaLotteryFour = Math.round(Math.random() * (53 - 1) + 1);
+    var floridaLotteryFive = Math.round(Math.random() * (53 - 1) + 1);
+    var floridaLotterySix = Math.round(Math.random() * (53 - 1) + 1);
+
+    //Powerball Numbers
     var powerballLottery = Math.round(Math.random() * (59 - 1) + 1);
+    var powerballLotteryTwo = Math.round(Math.random() * (59 - 1) + 1);
+    var powerballLotteryThree = Math.round(Math.random() * (59 - 1) + 1);
+    var powerballLotteryFour = Math.round(Math.random() * (59 - 1) + 1);
+    var powerballLotteryFive = Math.round(Math.random() * (59 - 1) + 1);
     var powerballNumber = Math.round(Math.random() * (35 - 1) + 1);
 
-    console.log(myRandomNumber);
-    console.log(floridaLottery);
-    console.log(powerballLottery);
-    console.log(powerballNumber);
+
+    if (lotteryChoice === "1") {
+        console.log(floridaLottery + "," + floridaLotteryTwo + "," + floridaLotteryThree + "," +  floridaLotteryFour + "," +  floridaLotteryFive + "," + floridaLotterySix );
+
+
+    }else if(lotteryChoice === "2"){
+            console.log(powerballLottery + "," + powerballLotteryTwo  + ',' + powerballLotteryThree + "," + powerballLotteryFour + "," + powerballLotteryFive + ",\nAnd the Powerball number is " + powerballNumber);
+    }else{
+            prompt("We could not understand your entry. Please try again later.");
+    }
+
 }
 
 
 
+
 // main code
-//lotteryChoice = lottoValidation(lotteryChoice); //This is calling the function that verifies that the user entered a choice.
-//console.log("you chose. " + lotteryChoice);
+lotteryChoice = lottoValidation(lotteryChoice); //This is calling the function that verifies that the user entered a choice.
 numberGen();
+
